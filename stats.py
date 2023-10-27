4beb4b81459a43e58b84b4784f8836d898194d32
Serge Lunev <lunarserge@gmail.com>
2023-10-27 13:28:39 -0700

    chart over time

--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
 PROJECT = ''
 
+import bisect
 from collections import Counter
 from datetime import date, timedelta
 import pickle
+import matplotlib.pyplot as plt
 from swin.GitHub import mapper
 
 with open(PROJECT+'.pickle', 'rb') as f:
@@ -11,17 +13,24 @@ with open(PROJECT+'.pickle', 'rb') as f:
 with open(PROJECT+'-affiliations.pickle', 'rb') as f:
     affiliations = pickle.load(f)
 
-def print_top(start_date, end_date):
+def _get_relevant(start_date, end_date):
     '''
-    Print top contributors for a range of dates.
+    Get PRs and affiliations for contributions within the specified range of dates.
     '''
-
-    # Get affiliations that contributed within the specified range of dates.
+    relevant_PRs = []
     relevant_affiliations = []
     for i in range(len(pulls)):
         pr = pulls[i]
         if start_date <= pr.closed_at.date() < end_date:
+            relevant_PRs.append(pulls[i])
             relevant_affiliations.append(affiliations[i])
+    return relevant_PRs, relevant_affiliations
+
+def print_top(start_date, end_date):
+    '''
+    Print top contributors for a range of dates.
+    '''
+    relevant_PRs,relevant_affiliations = _get_relevant(start_date, end_date)
 
     # Get top contributors and combine all others into 'other' affiliation.
     # Looking at 5 top contributors and potentially more if several contributed on the same level.
@@ -38,6 +47,48 @@ def print_top(start_date, end_date):
     for player,amount in Counter(relevant_affiliations).most_common():
         print(f'  - {mapper.get_full_entity_name(player)}: {amount}')
 
+def _get_one_timestamp(pr):
+    '''
+    Get the closing year and month of a pull request.
+    '''
+    closed = pr.closed_at
+    return date(closed.year,closed.month,1)
+
+def draw_PRs(start_date, end_date, contributors):
+    '''
+    Draw a chart of contribution over time.
+    '''
+    relevant_PRs,relevant_affiliations = _get_relevant(start_date, end_date)
+    min_stamp = max_stamp = _get_one_timestamp(relevant_PRs[0])
+
+    # Determine range limits.
+    for pr in relevant_PRs[1:]:
+        stamp = _get_one_timestamp(pr)
+        if stamp > max_stamp:
+            max_stamp = stamp
+        elif stamp < min_stamp:
+            min_stamp = stamp
+
+    # Create a range of dates based on the limits.
+    stamp = min_stamp
+    timestamps = [stamp]
+    while stamp != max_stamp:
+        stamp = date(stamp.year,stamp.month+1,1) if stamp.month < 12 else date(stamp.year+1,1,1)
+        timestamps.append(stamp)
+
+    fig, axes = plt.subplots()
+    axes.set_title('# Contributed PRs (monthly)')
+
+    for contributor in contributors:
+        amount = [0] * len(timestamps)
+        for i in range(len(relevant_PRs)):
+            if relevant_affiliations[i] == contributor:
+                amount[bisect.bisect(timestamps,relevant_PRs[i].closed_at.date())-1] += 1
+        axes.plot(timestamps,amount)
+    axes.legend(contributors)
+
+    fig.savefig('contribution')
+
 today = date.today()
 this_month = date(today.year, today.month, 1)
 last_month = date(this_month.year,this_month.month-1,1) if this_month.month > 1 \
@@ -46,3 +97,5 @@ last_year  = date(this_month.year-1, this_month.month, 1)
 
 print_top(last_year, this_month)
 print_top(last_month, this_month)
+
+draw_PRs(last_year, this_month, [])
